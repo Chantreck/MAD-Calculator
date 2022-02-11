c7594e0a598c771a2d07c381fab4addf3433131f
Chantreck <vladnetaev@mail.ru>
2022-02-12 01:18:10 +0700

    Added all elements

--- app/src/main/java/com/tsu/calculator/MainActivity.kt
+++ app/src/main/java/com/tsu/calculator/MainActivity.kt
@@ -1,40 +1,379 @@
 package com.tsu.calculator
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.tsu.calculator.ui.theme.CalculatorTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tsu.calculator.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            UI()
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+private fun UI() {
+    Column(
+        modifier = Modifier
+            .background(Background)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        Text(
+            text = "Calculator",
+            fontSize = 28.sp,
+            fontFamily = museo,
+            modifier = Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp)
+        )
+
+        Box(modifier = Modifier.padding(top = 24.dp, start = 24.dp, end = 24.dp)) {
+            Image(
+                painter = painterResource(R.drawable.result_bg),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Text(
+                text = "8888888888",
+                color = NumbersBackground,
+                fontFamily = digitalNumbers,
+                fontSize = 37.sp,
+                modifier = Modifier.padding(top = 23.dp, start = 22.dp)
+            )
+            Text(
+                text = "0123456789",
+                fontFamily = digitalNumbers,
+                fontSize = 37.sp,
+                modifier = Modifier.padding(top = 23.dp, start = 22.dp)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(top = 24.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+        ) {
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "AC",
+                    style = buttonTextStyle
+                )
+            }
+
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "+/-",
+                    style = buttonTextStyle
+                )
+            }
+
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "%",
+                    style = buttonTextStyle
+                )
+            }
+
+            Button(
+                onClick = {},
+                colors = buttonColors(backgroundColor = AccentBlue),
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "÷",
+                    style = accentButtonTextStyle
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+        ) {
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "7",
+                    style = buttonTextStyle
+                )
+            }
+
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "8",
+                    style = buttonTextStyle
+                )
+            }
+
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "9",
+                    style = buttonTextStyle
+                )
+            }
+
+            Button(
+                onClick = {},
+                colors = buttonColors(backgroundColor = AccentBlue),
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "X",
+                    style = accentButtonTextStyle
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+        ) {
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "4",
+                    style = buttonTextStyle
+                )
+            }
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "5",
+                    style = buttonTextStyle
+                )
+            }
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "6",
+                    style = buttonTextStyle
+                )
+            }
+            Button(
+                onClick = {},
+                colors = buttonColors(backgroundColor = AccentBlue),
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "–",
+                    style = accentButtonTextStyle
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+        ) {
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "1",
+                    style = buttonTextStyle
+                )
+            }
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "2",
+                    style = buttonTextStyle
+                )
+            }
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "3",
+                    style = buttonTextStyle
+                )
+            }
+            Button(
+                onClick = {},
+                colors = buttonColors(backgroundColor = AccentBlue),
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "+",
+                    style = accentButtonTextStyle
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 64.dp)
+                .fillMaxWidth()
+        ) {
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(2f, fill = true)
+                    .aspectRatio(11f / 5)
+            ) {
+                Text(
+                    text = "0",
+                    style = buttonTextStyle
+                )
+            }
+            TextButton(
+                onClick = {},
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = ",",
+                    style = buttonTextStyle
+                )
+            }
+            Button(
+                onClick = {},
+                colors = buttonColors(backgroundColor = AccentBlue),
+                shape = RoundedCornerShape(25),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f, fill = true)
+                    .aspectRatio(1f)
+            ) {
+                Text(
+                    text = "=",
+                    style = accentButtonTextStyle
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private val buttonTextStyle = TextStyle(
+    fontFamily = montserrat,
+    color = AccentBlue,
+    fontSize = 29.sp,
+    textAlign = TextAlign.Center
+)
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalculatorTheme {
-        Greeting("Android")
-    }
-}
+private val accentButtonTextStyle = TextStyle(
+    fontFamily = montserrat,
+    color = Color.White,
+    fontSize = 29.sp,
+    textAlign = TextAlign.Center
+)
---
+++ app/src/main/java/com/tsu/calculator/ui/theme/Font.kt
@@ -0,0 +1,10 @@
+package com.tsu.calculator.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.tsu.calculator.R
+
+val museo = FontFamily(Font(R.font.museo))
+val digitalNumbers = FontFamily(Font(R.font.digital_numbers))
+val montserrat = FontFamily(Font(R.font.montserrat))
