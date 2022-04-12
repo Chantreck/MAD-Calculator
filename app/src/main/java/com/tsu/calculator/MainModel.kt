4f2f6d1b3e60a8c61ace2d2d71689b58757fb761
Chantreck <vladnetaev@mail.ru>
2022-04-12 23:52:40 +0700

    Moved business logic to model layer

--- app/src/main/java/com/tsu/calculator/MainModel.kt
+++ app/src/main/java/com/tsu/calculator/MainModel.kt
@@ -1,28 +1,34 @@
 package com.tsu.calculator
 
-object MainModel {
-    fun calculatePercent(first: Double): Double {
-        return first / 100.0
+class MainModel {
+    private var commaAdded = false
+
+    private var _firstOperand: String = ""
+    private var _secondOperand: String = ""
+    private var _operation: Buttons? = null
+
+    companion object {
+        const val MAX_LENGTH = 10
     }
 
-    fun calculate(first: Double, second: Double, operator: String): Double {
+    private fun calculate(first: Double, second: Double, operator: Buttons): Double {
         val result: Double
 
         when (operator) {
-            "divide" -> {
+            Buttons.DIVIDE -> {
                 if (second.equals(0.0)) {
                     throw Exception("Division by zero")
                 } else {
                     result = first.div(second)
                 }
             }
-            "multiply" -> {
+            Buttons.MULTIPLY -> {
                 result = first * second
             }
-            "subtract" -> {
+            Buttons.MINUS -> {
                 result = first - second
             }
-            "add" -> {
+            Buttons.PLUS -> {
                 result = first + second
             }
             else -> {
@@ -31,5 +37,143 @@ object MainModel {
         }
 
         return result
+    }
+
+    fun getResult(): String {
+        val operation = _operation
+        if (operation == null) {
+            if (_firstOperand.isEmpty()) {
+                _firstOperand = _secondOperand
+                clearSecondOperand()
+                return ""
+            } else {
+                clear()
+                return "Error"
+            }
+        }
+
+        if (_firstOperand.isNotEmpty() && _secondOperand.isNotEmpty()) {
+            val firstNumber = _firstOperand.toDouble()
+            val secondNumber = _secondOperand.toDouble()
+
+            try {
+                val result = calculate(firstNumber, secondNumber, operation)
+                _firstOperand = result.toString()
+                clearSecondOperand()
+                return result.toString()
+            } catch (e: Exception) {
+                clear()
+                return "Error"
+            }
+        }
+
+        _firstOperand = _secondOperand
+        clearSecondOperand()
+        return ""
+    }
+
+    fun setOperation(operation: Buttons): String {
+        if (_firstOperand.isEmpty() && _secondOperand.isEmpty()) {
+            _firstOperand = "0"
+        }
+
+        if (_secondOperand.isNotEmpty()) {
+            val result = getResult()
+            _operation = operation
+            return result
+        }
+
+        _operation = operation
+        return ""
+    }
+
+    private fun clearSecondOperand() {
+        commaAdded = false
+        _secondOperand = ""
+    }
+
+    fun clear() {
+        _firstOperand = ""
+        clearSecondOperand()
+        _operation = null
+    }
+
+    fun changeSign(): String {
+        if ((_secondOperand.isEmpty() || _secondOperand == "0") &&
+            (_firstOperand.isEmpty() || _firstOperand == "0")
+        ) {
+            return ""
+        }
+
+        if (_secondOperand.isEmpty() || _secondOperand == "0") {
+            if (_firstOperand[0] == '-') {
+                _firstOperand = _firstOperand.drop(1)
+            } else {
+                _firstOperand = "-$_firstOperand"
+            }
+            return _firstOperand
+        }
+
+        if (_secondOperand[0] == '-') {
+            _secondOperand = _secondOperand.drop(1)
+        } else {
+            _secondOperand = "-$_secondOperand"
+        }
+        return _secondOperand
+    }
+
+    fun addNumber(number: String): String {
+        val length = _secondOperand.length
+        if (length >= MAX_LENGTH - 1) {
+            return ""
+        }
+
+        if (_secondOperand.isEmpty() || (_secondOperand == "0" && number == "0")) {
+            _secondOperand = number
+        } else {
+            _secondOperand += number
+        }
+
+        return _secondOperand
+    }
+
+    fun addComma(): String {
+        val length = _secondOperand.length
+        if (length >= MAX_LENGTH - 1) {
+            return ""
+        }
+
+        if (_secondOperand.isEmpty()) {
+            _secondOperand = "0."
+            commaAdded = true
+        } else {
+            if (commaAdded) {
+                return ""
+            }
+            _secondOperand += "."
+            commaAdded = true
+        }
+
+        return _secondOperand
+    }
+
+    fun getPercentage(): String {
+        val number: String = when {
+            _firstOperand.isNotEmpty() -> {
+                _firstOperand
+            }
+            _secondOperand.isNotEmpty() -> {
+                _secondOperand
+            }
+            else -> {
+                return ""
+            }
+        }
+
+        val result = number.toDouble() / 100.0
+        _firstOperand = result.toString()
+        clearSecondOperand()
+
+        return _firstOperand
     }
 }
--- app/src/main/java/com/tsu/calculator/MainViewModel.kt
+++ app/src/main/java/com/tsu/calculator/MainViewModel.kt
@@ -1,18 +1,11 @@
 package com.tsu.calculator
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlin.math.abs
-import kotlin.math.floor
 
 class MainViewModel : ViewModel() {
-    private var commaAdded = false
-
-    private var _firstOperand: String = ""
-    private var _secondOperand: String = ""
-    private var _operation: String? = null
+    private val model = MainModel()
 
     private var _display = MutableLiveData<String>()
     val display: LiveData<String> get() = _display
@@ -22,157 +15,56 @@ class MainViewModel : ViewModel() {
     }
 
     fun action(button: Buttons) {
-
-    }
-
-    fun addNumber(number: String) {
-        val length = _secondOperand.length
-        if (length >= 9) {
-            return
+        val result: String
+        when (button) {
+            Buttons.CLEAR -> {
+                model.clear()
+                result = "0"
+            }
+            Buttons.PLUS_MINUS -> {
+                result = model.changeSign()
+            }
+            Buttons.PERCENT -> {
+                result = model.getPercentage()
+            }
+            Buttons.DIVIDE, Buttons.MULTIPLY, Buttons.MINUS, Buttons.PLUS -> {
+                var temp = model.setOperation(button)
+                if (temp.endsWith(".0")) {
+                    temp = temp.dropLast(2)
+                }
+                result = temp
+            }
+            Buttons.EQUAL -> {
+                var temp = model.getResult()
+                if (temp.endsWith(".0")) {
+                    temp = temp.dropLast(2)
+                }
+                result = temp
+            }
+            Buttons.COMMA -> {
+                result = model.addComma()
+            }
+            else -> {
+                result = model.addNumber(button.label)
+            }
         }
-
-        if (_secondOperand.isEmpty()) {
-            if (number == ".") {
-                _secondOperand = "0."
-                commaAdded = true
-            } else {
-                _secondOperand = number
-            }
-        } else {
-            if (number == "." && commaAdded) {
-                return
-            }
-            if (number == ".") {
-                commaAdded = true
-            }
-            _secondOperand += number
-        }
-
-        prepareDisplayValue(_secondOperand)
+        prepareDisplayValue(result)
     }
 
-    private fun clearSecondOperand() {
-        commaAdded = false
-        _secondOperand = ""
-    }
-
-    fun clear() {
-        _firstOperand = ""
-        clearSecondOperand()
-
-        _operation = null
+    private fun clear() {
+        model.clear()
         prepareDisplayValue("0")
     }
 
-    fun setOperation(operation: String) {
-        if (_firstOperand.isEmpty() && _secondOperand.isEmpty()) {
-            _firstOperand = "0"
-        }
-
-        if (operation == "percent") {
-            getPercentage()
-        }
-
-        if (_secondOperand.isNotEmpty()) {
-            getResult()
-        }
-        _operation = operation
-    }
-
-    private fun getPercentage() {
-        val number: String = when {
-            _firstOperand.isNotEmpty() -> {
-                _firstOperand
-            }
-            _secondOperand.isNotEmpty() -> {
-                _secondOperand
-            }
-            else -> {
-                return
-            }
-        }
-
-        val result = MainModel.calculatePercent(number.toDouble())
-        _firstOperand = result.toString()
-        prepareDisplayValue(_firstOperand)
-        clearSecondOperand()
-        return
-    }
-
-    fun getResult() {
-        val operation = _operation
-        if (operation == null) {
-            if (_firstOperand.isEmpty()) {
-                _firstOperand = _secondOperand
-                clearSecondOperand()
-                return
-            } else {
-                clear()
-                prepareDisplayValue("Error")
-                return
-            }
-        }
-
-        if (_firstOperand.isNotEmpty() && _secondOperand.isNotEmpty()) {
-            val firstNumber = _firstOperand.toDouble()
-            val secondNumber = _secondOperand.toDouble()
-
-            try {
-                val result = MainModel.calculate(firstNumber, secondNumber, operation)
-                _firstOperand = result.toString()
-                prepareDisplayValue(result.toString())
-                clearSecondOperand()
-
-                Log.d("Test", "$firstNumber $operation $secondNumber = $result")
-            } catch (e: Exception) {
-                clear()
-                prepareDisplayValue("Error")
-
-                Log.e("Test", e.message.toString())
-            }
-
-            return
-        }
-
-        _firstOperand = _secondOperand
-        clearSecondOperand()
-    }
-
-    fun changeSign() {
-        if ((_secondOperand.isEmpty() || _secondOperand == "0") &&
-            (_firstOperand.isEmpty() || _firstOperand == "0")
-        ) {
-            return
-        }
-
-        if (_secondOperand.isEmpty() || _secondOperand == "0") {
-            if (_firstOperand[0] == '-') {
-                _firstOperand = _firstOperand.drop(1)
-            } else {
-                _firstOperand = "-" + _firstOperand
-            }
-            prepareDisplayValue(_firstOperand)
-        } else {
-            if (_secondOperand[0] == '-') {
-                _secondOperand = _secondOperand.drop(1)
-            } else {
-                _secondOperand = "-" + _secondOperand
-            }
-            prepareDisplayValue(_secondOperand)
-        }
-    }
-
     private fun prepareDisplayValue(value: String) {
-        var result = value
-
-        if (result.endsWith(".0")) {
-            result = result.dropLast(2)
+        if (value.isEmpty()) {
+            return
         }
 
-        if (result.length > 9) {
-            _display.value = String.format("%.5E", result.toDouble())
+        if (value.length > MainModel.MAX_LENGTH) {
+            _display.value = String.format("%.5E", value.toDouble())
         } else {
-            _display.value = result
+            _display.value = value
         }
     }
 }
